dbe178b8f0bab4feefc3c8061c902dd0da4d8684
Laingemaguzu <72946384+Laingemaguzu@users.noreply.github.com>
2020-10-15 15:20:58 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.clearScreen()
+    } else {
+        if (input.buttonIsPressed(Button.A)) {
+            basic.showLeds(`
+                . . # . .
+                . # # . .
+                # # # # #
+                . # # . .
+                . . # . .
+                `)
+        }
+        basic.clearScreen()
+        if (input.buttonIsPressed(Button.B)) {
+            basic.showLeds(`
+                . . # . .
+                . . # # .
+                # # # # #
+                . . # # .
+                . . # . .
+                `)
+            basic.clearScreen()
+        }
+    }
+})
